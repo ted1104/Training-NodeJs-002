ba5ba73759cb2ea9c6a9e2780d218bb1ceb4ed9c
Teddy Walter <teddywalter2016@gmail.com>
2022-02-26 00:35:43 +0200

    blocking code

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const _ = require("lodash");
+const http = require("http");
 
-const items = [1, [2, [3, [4, [5]]]]];
-const newItems = _.flatMapDeep(items);
-
-console.log(newItems);
-console.log("Hello mon frere Teddy Walter");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("HOME PAGE");
+    return;
+  }
+  if (req.url === "/about") {
+    //blocking code
+    for (let i = 0; i < 1000; i++) {
+      for (let j = 0; j < 1000; j++) {
+        console.log(`${i} ${j}`);
+      }
+    }
+    res.end("ABOUT PAGE");
+    return;
+  }
+  res.end("ERROR PAGE");
+  //   res.end(`${req.url}`);
+});
+server.listen(5000, () => {
+  console.log("Listening on port 5000 ...");
+});
