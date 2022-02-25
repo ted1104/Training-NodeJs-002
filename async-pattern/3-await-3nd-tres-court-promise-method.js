07fd840163844bfec6f146f6067e1c0a0a925d46
Teddy Walter <teddywalter2016@gmail.com>
2022-02-26 01:54:30 +0200

    3rd method to call promise

--- async-pattern/3-await-3nd-tres-court-promise-method.js
+++ async-pattern/3-await-3nd-tres-court-promise-method.js
@@ -1,16 +1,17 @@
-const { writeFile, readFile } = require("fs");
-const util = require("util");
+const { writeFile, readFile } = require("fs").promises;
+// const util = require("util");
 
-const readFilePromise = util.promisify(readFile);
-const writeFilePromise = util.promisify(writeFile);
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
 const start = async () => {
   try {
-    const first = await readFilePromise("./content/first.txt", "utf-8");
-    const second = await readFilePromise("./content/second.txt", "utf-8");
-    await writeFilePromise(
-      "./content/result-mind-await-second.txt",
-      `THIS AWESOME ${first} ${second}`
+    const first = await readFile("./content/first.txt", "utf-8");
+    const second = await readFile("./content/second.txt", "utf-8");
+    await writeFile(
+      "./content/result-mind-await-third.txt",
+      `THIS AWESOME ${first} ${second}`,
+      { flag: "a" }
     );
     console.log(first, second);
   } catch (err) {
